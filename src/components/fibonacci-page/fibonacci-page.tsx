cf25a729c2dbc380281af78326e6b41f00a8dd24
Karkachevich <karkachevichtv@yandex.ru>
2022-10-08 16:55:37 +0500

    feat: add input button circle

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./fibonacci-page.module.css";
+import { Input } from "../ui/input/input";
+import { Circle } from "../ui/circle/circle";
+import { Button } from "../ui/button/button";
 
 export const FibonacciPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const [inProgress, setInProgress] = useState<boolean>(false);
+
+  const onChange = (evt: React.SyntheticEvent<HTMLInputElement, Event>) => {
+    const element = evt.currentTarget.value;
+    setInputValue(element);
+  };
+
+  
+
   return (
-    <SolutionLayout title="Последовательность Фибоначчи"/>
+    <SolutionLayout title="Последовательность Фибоначчи">
+      <div className={styles.container}>
+        <Input
+          type=""
+          max={19}
+          extraClass={styles.input}
+          isLimitText={true}
+          onChange={onChange}
+        />
+        <Button text="Развернуть" extraClass={styles.button} />
+      </div>
+      <ul className={styles.circles}>{inputValue && <Circle />}</ul>
+    </SolutionLayout>
   );
 };
