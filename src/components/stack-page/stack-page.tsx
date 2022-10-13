5ddf8c6f33ee29a749a911bb0270cbca7f962f38
Karkachevich <karkachevichtv@yandex.ru>
2022-10-13 13:40:58 +0500

    feat: add Input Button

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { FC } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
 
-export const StackPage: React.FC = () => {
+export const StackPage: FC = () => {
   return (
-    <SolutionLayout title="Стек"/>
+    <SolutionLayout title="Стек">
+      <Input />
+      <Button />
+      <Button />
+      <Button />
+    </SolutionLayout>
   );
 };
