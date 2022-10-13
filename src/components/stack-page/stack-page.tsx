a2f62d8e4ba57b64504f62f9fa3d3d85f396f549
Karkachevich <karkachevichtv@yandex.ru>
2022-10-13 14:00:21 +0500

    feat: add file styles

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -2,14 +2,22 @@ import React, { FC } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import styles from "./stack-page.module.css";
 
 export const StackPage: FC = () => {
   return (
     <SolutionLayout title="Стек">
-      <Input />
-      <Button />
-      <Button />
-      <Button />
+      <div className={styles.container}>
+        <Input extraClass={styles.input} />
+        <Button text="Добавить" extraClass={styles.button_add}/>
+        <Button text="Удалить" extraClass={styles.button_remove}/>
+        <Button text="Очистить"/>
+      </div>
+      <div className={styles.circles}>
+      <Circle extraClass={styles.circle}/>
+      </div>
+      
     </SolutionLayout>
   );
 };
