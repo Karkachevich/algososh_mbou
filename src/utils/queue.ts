f346e49cf0affc91cf7fc7f537fd19e6acd9d7f9
Karkachevich <karkachevichtv@yandex.ru>
2022-10-14 14:50:26 +0500

    feat: add getHead getTail

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -1,45 +1,53 @@
 interface IQueue<T> {
-    enqueue: (item: T) => void;
-    dequeue: () => void;
-    peak: () => T | null;
+  enqueue: (item: T) => void;
+  dequeue: () => void;
+  peak: () => T | null;
+  getHead: () => { value: T | null; index: number } ;
+  getTail: () => { value: T | null; index: number } ;
+}
+
+export class Queue<T> implements IQueue<T> {
+  private container: (T | null)[] = [];
+  private head = 0;
+  private tail = 0;
+  private readonly size: number = 0;
+  private length: number = 0;
+
+  constructor(size: number) {
+    this.size = size;
+    this.container = Array(size);
   }
-  
-  export class Queue<T> implements IQueue<T> {
-    private container: (T | null)[] = [];
-    private head = 0;
-    private tail = 0;
-    private readonly size: number = 0;
-    private length: number = 0;
-  
-    constructor(size: number) {
-      this.size = size;
-      this.container = Array(size);
+  getHead = () => {
+    return {value: this.container[this.head], index: this.head};
+  };
+  getTail = () => {
+    return {value: this.container[this.tail], index: this.tail};
+  };
+
+  enqueue = (item: T) => {
+    if (this.length >= this.size) {
+      throw new Error("Maximum length exceeded");
     }
-  
-    enqueue = (item: T) => {
-      if (this.length >= this.size) {
-        throw new Error("Maximum length exceeded");
-      }
-        this.container[this.tail % this.size] = item;
-        this.tail++;
-        this.length++;
-    };
-  
-    dequeue = () => {
-      if (this.isEmpty()) {
-        throw new Error("No elements in the queue");
-      }
-        delete this.container[this.head % this.size];
-        this.head++;
-        this.length--;
-    };
-  
-    peak = (): T | null => {
-      if (this.isEmpty()) {
-        throw new Error("No elements in the queue");
-      }
-     return this.container[this.head % this.size];
-    };
-  
-    isEmpty = () => this.length === 0;
-  }
+    this.container[this.tail % this.size] = item;
+    this.tail++;
+    this.length++;
+  };
+
+  dequeue = () => {
+    if (this.isEmpty()) {
+      throw new Error("No elements in the queue");
+    }
+    delete this.container[this.head % this.size];
+    this.head++;
+    this.length--;
+  };
+
+  peak = (): T | null => {
+    if (this.isEmpty()) {
+      throw new Error("No elements in the queue");
+    }
+    return this.container[this.head % this.size];
+  };
+
+  isEmpty = () => this.length === 0;
+}
