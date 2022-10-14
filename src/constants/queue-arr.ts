2a364b15441deb6e53c83e6189d3e47c072ecc20
Karkachevich <karkachevichtv@yandex.ru>
2022-10-14 13:37:25 +0500

    feat: add queue-arr

---
+++ src/constants/queue-arr.ts
@@ -0,0 +1,12 @@
+import { ElementStates } from "../types/element-states";
+import { TChar } from "../types/char";
+
+export const queueArr: TChar[] = [
+    { char: "", state: ElementStates.Default, head: "", tail: "" },
+    { char: "", state: ElementStates.Default, head: "", tail: "" },
+    { char: "", state: ElementStates.Default, head: "", tail: "" },
+    { char: "", state: ElementStates.Default, head: "", tail: "" },
+    { char: "", state: ElementStates.Default, head: "", tail: "" },
+    { char: "", state: ElementStates.Default, head: "", tail: "" },
+    { char: "", state: ElementStates.Default, head: "", tail: "" },
+  ];
