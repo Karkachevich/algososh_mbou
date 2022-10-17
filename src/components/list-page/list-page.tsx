989ccee108314c16eb9e97c6c75ab541175ee60b
Karkachevich <karkachevichtv@yandex.ru>
2022-10-17 16:47:10 +0500

    feat: add randomArr

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,11 +1,21 @@
-import React, {FC} from "react";
+import React, { FC, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
 import styles from "./list-page.module.css";
+import { ElementStates } from "../../types/element-states";
+import { TChar } from "../../types/char";
 
 export const ListPage: FC = () => {
+  const size: number = 4;
+  const randomArr: TChar[] = [...Array(size)].map(() => ({
+    char: String(Math.floor(Math.random() * 100)),
+    state: ElementStates.Default,
+  }));
+  const [charsArr, setCharsArr] = useState<TChar[]>(randomArr);
+
   return (
     <SolutionLayout title="Связный список">
       <div className={styles.container}>
@@ -26,9 +36,22 @@ export const ListPage: FC = () => {
         <Button text="Удалить по индексу" extraClass={styles.button_index} />
       </div>
       <div className={styles.circles}>
-      <Circle />
+        {charsArr.map((item, index) => {
+          return (
+            <li key={index} className={styles.items}>
+              <Circle
+                state={item.state}
+                extraClass={styles.circle}
+                letter={item.char}
+                index={index}
+                head={item.head}
+                tail={item.tail}
+              />
+              {charsArr.length > index + 1 && <ArrowIcon/>}
+            </li>
+          );
+        })}
       </div>
-      
     </SolutionLayout>
   );
 };
