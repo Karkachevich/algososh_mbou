70e2a06f3a5b77539294facb9c8eeec0c5df99c1
Karkachevich <karkachevichtv@yandex.ru>
2022-10-17 15:11:15 +0500

    feat: add styles

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, {FC} from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
+import styles from "./list-page.module.css";
 
-
-export const ListPage: React.FC = () => {
+export const ListPage: FC = () => {
   return (
     <SolutionLayout title="Связный список">
-      <Input/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Button/>
-      <Input/>
-      <Button/>
-      <Button/>
+      <div className={styles.container}>
+        <Input
+          type="text"
+          maxLength={4}
+          isLimitText={true}
+          extraClass={styles.input}
+        />
+        <Button text="Добавить в head" extraClass={styles.button_list} />
+        <Button text="Добавить в tail" extraClass={styles.button_list} />
+        <Button text="Удалить из head" extraClass={styles.button_list} />
+        <Button text="Удалить из tail" extraClass={styles.button_list} />
+      </div>
+      <div className={styles.container}>
+        <Input extraClass={styles.input} />
+        <Button text="Добавить по индексу" extraClass={styles.button_index} />
+        <Button text="Удалить по индексу" extraClass={styles.button_index} />
+      </div>
+      <div className={styles.circles}>
+      <Circle />
+      </div>
+      
     </SolutionLayout>
   );
 };
