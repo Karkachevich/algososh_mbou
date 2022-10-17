a67d0ae4893a8a00315a38255a2b3a57d5c923fa
Karkachevich <karkachevichtv@yandex.ru>
2022-10-17 14:43:53 +0500

    feat: add Input Button Circle

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+
 
 export const ListPage: React.FC = () => {
   return (
-    <SolutionLayout title="Связный список"/>
+    <SolutionLayout title="Связный список">
+      <Input/>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Input/>
+      <Button/>
+      <Button/>
+    </SolutionLayout>
   );
 };
