3000bb632f2f0e83433dc50f059691611c6ee42c
Karkachevich <karkachevichtv@yandex.ru>
2022-10-17 19:19:23 +0500

    feat: add class LinkedList

---
+++ src/utils/linked-list.ts
@@ -0,0 +1,84 @@
+export class Node<T> {
+  value: T;
+  next: Node<T> | null;
+  constructor(value: T, next?: Node<T> | null) {
+    this.value = value;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+interface ILinkedList<T> {
+  append: (element: T) => void;
+  insertAt: (element: T, position: number) => void;
+  getSize: () => number;
+  print: () => void;
+}
+
+export class LinkedList<T> implements ILinkedList<T> {
+  private head: Node<T> | null;
+  private size: number;
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  insertAt(element: T, index: number) {
+    if (index < 0 || index > this.size) {
+      console.log("Enter a valid index");
+      return;
+    } else {
+      const node = new Node(element);
+
+      if (index === 0) {
+        node.next = this.head;
+        this.head = node;
+      } else {
+        let curr = this.head;
+        let currIndex = 0;
+        let prev = null;
+
+        while (currIndex < index && curr) {
+          prev = curr;
+          curr = curr.next;
+          currIndex++;
+        }
+
+        if (prev) prev.next = node;
+        node.next = curr;
+      }
+
+      this.size++;
+    }
+  }
+
+  append(element: T) {
+    const node = new Node(element);
+    let current;
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  print() {
+    let curr = this.head;
+    let res = "";
+    while (curr) {
+      res += `${curr.value} `;
+      curr = curr.next;
+    }
+    
+  }
+}
